3db3114b5f70585bf0578c204413d2a2a789c0e5
Ng Boon Leng <Ng_Boon_Leng@yahoo.com>
2024-06-03 17:14:45 +0800

    Update firebase error handle for LoginPage and SignUp page

--- src/views/SignUpPage.js
+++ src/views/SignUpPage.js
@@ -10,6 +10,40 @@ export default function SignUpPage() {
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
+  const handleFirebaseError = (message) => {
+    if (message.includes("auth/email-already-in-use")) {
+      setError("The email address is already in use by another account.");
+    } else if (message.includes("auth/invalid-email")) {
+      setError("The email address is badly formatted.");
+    } else if (message.includes("auth/operation-not-allowed")) {
+      setError("Email/password accounts are not enabled.");
+    } else if (message.includes("auth/weak-password")) {
+      setError("Password should be at least 6 characters.");
+    } else if (message.includes("auth/internal-error")) {
+      setError("An internal error has occurred.");
+    } else if (message.includes("auth/network-request-failed")) {
+      setError("A network error has occurred. Please try again.");
+    } else if (message.includes("auth/too-many-requests")) {
+      setError("Too many requests. Try again later.");
+    } else if (message.includes("auth/invalid-api-key")) {
+      setError("Your API key is invalid.");
+    } else if (message.includes("auth/app-deleted")) {
+      setError("This instance of FirebaseApp has been deleted.");
+    } else if (message.includes("auth/invalid-user-token")) {
+      setError(
+        "The user's credential is no longer valid. Please sign in again."
+      );
+    } else if (message.includes("auth/user-token-expired")) {
+      setError("The user's credential has expired. Please sign in again.");
+    } else if (message.includes("auth/web-storage-unsupported")) {
+      setError(
+        "This browser is not supported or 3rd party cookies and data may be disabled."
+      );
+    } else {
+      setError("An unknown error occurred.");
+    }
+  };
+
   return (
     <Container>
       <h1 className="my-3">Sign up for an account</h1>
@@ -47,7 +81,7 @@ export default function SignUpPage() {
                 await createUserWithEmailAndPassword(auth, username, password);
                 navigate("/");
               } catch (error) {
-                setError(error.message);
+                handleFirebaseError(error.message);
               }
             }
           }}
