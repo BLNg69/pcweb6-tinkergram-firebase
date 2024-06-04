04dd132e046fc71b0b43b8ce7ac30d729b532d5b
Ng Boon Leng <Ng_Boon_Leng@yahoo.com>
2024-06-04 13:01:31 +0800

    Remove the "Sign Out" link in Home page Nav Bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,13 +3,14 @@ import { signOut } from "firebase/auth";
 import { Container, Nav, Navbar } from "react-bootstrap";
 
 export default function Navigation() {
+  const isHomePage = window.location.pathname === '/';
   return (
     <Navbar variant="dark" bg="dark">
       <Container>
-        <Navbar.Brand href="/">Tinkergram</Navbar.Brand>
+      <Navbar.Brand href="/">Tinkergram</Navbar.Brand>
         <Nav>
           <Nav.Link href="/add">New Post</Nav.Link>
-          <Nav.Link onClick={(e) => signOut(auth)}>Sign Out</Nav.Link>
+          {isHomePage ? null : <Nav.Link onClick={(e) => signOut(auth)}>Sign Out</Nav.Link>}
         </Nav>
       </Container>
     </Navbar>
